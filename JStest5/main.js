698f4c356635268186b5fd3778bd5bc721b1d14e
junho-myeong <wnsgh9166@naver.com>
2022-07-06 14:10:30 +0900

    js

---
+++ JStest5/main.js
@@ -0,0 +1,20 @@
+// 문자 자료형
+// let myName = 'junho';
+// let eMail = 'wnsgh9166@naver.com';
+// // 벡틱 기호를 사용한 보관법($사인을 통해)은 데이터를 넣어서 문자로 사용하기 위한 방법 중에 하나이다, $사이에 데이터를 넣어서 문자로 사용하기 위한 방법이다.
+// let hello = `hello ${myName}?!`;
+
+// console.log(myName);
+// console.log(eMail);
+// console.log(hello);
+
+
+// 객체 자료형(객체 데이터들은 그 안에 여러가지 데이터들을 가지고 잇고, .을 사용해서 그 값을 얻을수 잇다.)
+let user = {
+  name: 'junho',
+  age: 23
+};
+
+console.log(user);
+console.log(user.name);
+console.log(user.age);
