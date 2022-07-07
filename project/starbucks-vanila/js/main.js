a18a501670fd7ac464320d874ffdea2e92d6b94d
junho-myeong <wnsgh9166@naver.com>
2022-07-07 16:49:37 +0900

    starbucks header sub-menu

---
+++ project/starbucks-vanila/js/main.js
@@ -0,0 +1,16 @@
+const searchEl = document.querySelector('header .search')
+const searchInput = searchEl.querySelector('input')
+
+searchEl.addEventListener('click', function() {
+  searchInput.focus();
+})
+
+searchInput.addEventListener('focus', function() {
+  searchEl.classList.add('focused');
+  searchInput.setAttribute('placeholder', '통합검색');
+})
+
+searchInput.addEventListener('blur', function() {
+  searchEl.classList.remove('focused');
+  searchInput.setAttribute('placeholder', '');
+})
