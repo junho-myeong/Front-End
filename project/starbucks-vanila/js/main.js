7ac50531a02d7d8981a60c7c87179378436356bb
junho-myeong <wnsgh9166@naver.com>
2022-07-14 15:04:39 +0900

    animation

--- project/starbucks-vanila/js/main.js
+++ project/starbucks-vanila/js/main.js
@@ -121,3 +121,16 @@ function floatingObject(selector, dleay, size) {
 floatingObject('.floating1', 1, 15)
 floatingObject('.floating2', .5, 15)
 floatingObject('.floating3', 1.5, 20)
+
+
+// find store
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function(spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 작성
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller())
+})
